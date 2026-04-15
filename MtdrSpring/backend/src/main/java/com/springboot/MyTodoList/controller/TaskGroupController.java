43678cdf7ac71020d2693082fc09bbba8e08a6b8
Gastlt <diegomacielpliego@gmail.com>
2026-04-15 00:42:38 -0600

    fix de CORS en los controllers

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/TaskGroupController.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/TaskGroupController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/taskgroups")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskGroupController {
 
     private final TaskGroupService service;
