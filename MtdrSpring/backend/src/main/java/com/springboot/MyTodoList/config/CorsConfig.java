42be7e7186ffdf173be45b291561ab7e4e737f0c
OmgWaffle3D <A01198876@tec.mx>
2026-04-21 18:02:15 -0600

    "BE para EP1"

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/CorsConfig.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/CorsConfig.java
@@ -13,11 +13,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
-                registry.addMapping("/**") 
-                        .allowedOrigins("http://localhost:3000")
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "http://192.168.0.11:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); 
+                        .allowCredentials(true);
             }
         };
     }
