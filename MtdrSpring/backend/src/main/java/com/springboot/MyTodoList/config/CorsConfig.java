704ab3fe8e887dd66ebaaa4e1976fab3dafd4731
MiguelGavito <mgavitogzz@gmail.com>
2026-05-30 08:25:11 -0600

    se reducio la seguridad, ahora es mas permisivo a la entrada de datos con url al backend

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/CorsConfig.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/CorsConfig.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origins:http://localhost:3000}")
+    @Value("${app.cors.allowed-origins:*}")
     private String allowedOriginsConfig;
 
     @Bean
